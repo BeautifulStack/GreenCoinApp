f2199404b0c41f9179f9d3777c89a26ee1a9a4e4
sven-eliasen <jo.pettersen@pm.me>
2021-06-10 18:20:36 +0200

    functional qr code reader

--- app/src/main/java/com/example/fairrepack/Send.java
+++ app/src/main/java/com/example/fairrepack/Send.java
@@ -1,77 +1,71 @@
 package com.example.fairrepack;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Send extends AppCompatActivity {
-    public static final int CAMERA_PERMISSION_CODE = 100;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button camera;
-    private String permission;
-    private int requestCode;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class Send extends AppCompatActivity implements View.OnClickListener {
     private Button scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-        camera = findViewById(R.id.camera);
+
         scan = findViewById(R.id.scan);
-
-        scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Send.this,ScanActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        camera.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
-            }
-        });
-    }
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void checkPermission(String permission, int requestCode){
-        this.permission = permission;
-        this.requestCode = requestCode;
-        if(ContextCompat.checkSelfPermission(Send.this, permission)
-                == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(Send.this, new String[] {permission},
-                    requestCode);
-        }
-        else {
-            Toast.makeText(this,"Permission Already Granted", Toast.LENGTH_SHORT).show();
-        }
+        scan.setOnClickListener(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == CAMERA_PERMISSION_CODE){
-            if(grantResults.length > 0
-            && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show();
+    public void onClick(View v) {
+        scanCode();
+    }
+
+    private void scanCode(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scanning code...");
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() != null){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Scanning Result");
+                builder.setMessage(result.getContents());
+                builder.setPositiveButton("Scan again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        scanCode();
+                    }
+                }).setNegativeButton("Finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                Toast.makeText(this, "No results", Toast.LENGTH_LONG).show();
             }
-            else {
-                Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT).show();
-            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
 }
