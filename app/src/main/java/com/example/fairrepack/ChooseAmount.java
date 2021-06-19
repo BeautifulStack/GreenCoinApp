8a5171dd153b8995e87efcd486a3f7b40b41d4d0
lealaroze <llaroze1@myges.fr>
2021-06-19 14:38:48 +0200

    Choosing Transfer Amount

---
+++ app/src/main/java/com/example/fairrepack/ChooseAmount.java
@@ -0,0 +1,22 @@
+package com.example.fairrepack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class ChooseAmount extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_amount);
+
+}}
